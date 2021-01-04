7cfcafa1a4f6eebfbaa2de32cf7d558d6067a556
johnpatrickanders <johnpatrickanders@gmail.com>
2021-01-04 16:33:59 -0500

    add models file but incomplete

--- api/api.py
+++ api/api.py
@@ -1,7 +1,9 @@
 import time
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 
 
 @app.route('/time')
---
+++ api/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    firstname = db.Column(db.String(40), nullable=False)
+    lastname = db.Column(db.String(40), nullable=False)
+    hashed_password = db.Column(db.String(100), nullable=False)
