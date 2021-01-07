cb7fb80495ac5c8981656e2db39a9b44b451f70c
johnpatrickanders <johnpatrickanders@gmail.com>
2021-01-06 20:07:36 -0500

    add react router dom and basic home route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home';
 import './App.css';
 
 function App() {
@@ -12,12 +18,31 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <logo />
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
