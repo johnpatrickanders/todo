81f14dc7f09b520ca031e8d656fe40eb7e827638
johnpatrickanders <johnpatrickanders@gmail.com>
2021-01-11 15:23:42 -0500

    working api

--- app_container/config.py
+++ app_container/config.py
@@ -6,3 +6,4 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_ECHO = True
+    FLASK_ENV = os.environ.get('FLASK_ENV')
