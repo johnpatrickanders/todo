a1c9042e63ea0038af7594c8235b9e6e54f47f75
johnpatrickanders <johnpatrickanders@gmail.com>
2021-02-02 10:52:23 -0500

    add trivially working dropdown

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './DropDown.css';
 
 export default function ({ createList }) {
+  const [showForm, setShowForm] = useState(false);
+
+  const pop = () => {
+    console.log("POP")
+    setShowForm(true);
+  }
 
   return (
     <div className="lists__dropdown">
-      <button onClick={createList} className="lists__button">Add List</button>
+      <button onClick={pop} className="lists__button">Add List</button>
       <div className="lists__dropdown-content">
-
+        {showForm ? <h1>
+          Hello There
+        </h1> : null}
       </div>
     </div>
   )
