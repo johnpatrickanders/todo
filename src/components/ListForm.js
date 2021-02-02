5b849975df66b329e167a71b38a705532ae20a25
johnpatrickanders <johnpatrickanders@gmail.com>
2021-02-02 14:31:33 -0500

    make dropdown disapper on button click

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 import './ListForm.css'
 
-export default function ({ createList }) {
+export default function ({ createList, setShowForm }) {
   const [title, setTitle] = useState("");
 
   return (
     <div className="dropdown-container">
       <input className="dropdown-input" name="list-entry" type="text" onChange={(e) => setTitle(e.target.value)}></input>
-      <button className="dropdown-button" onClick={() => createList(title)}>Create</button>
+      <button className="dropdown-button" onClick={() => {
+        createList(title);
+        setShowForm(false);
+      }}>Create</button>
     </div>
   )
 }
