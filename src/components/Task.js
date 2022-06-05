0a5038b2a5f371ab2cbe51c4c8607702906ae737
johnpatrickanders@gmail.com <66134557+johnpatrickanders@users.noreply.github.com>
2022-06-04 20:41:17 -0400

    add hover effect

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './Task.css';
 
 export default function ({ task }) {
-  // const [done, setDone] = useState(task.status);
-  // const [taskState, setTaskState] = useState(task)
   const [classNames, setClassNames] = useState("tasks__task");
   const handleClick = () => {
-    // setDone(!done);
     task.status = task.status === 'Open' ? 'Complete' : 'Open';
     setClassNames(`tasks__task ${task.status === 'Complete' ? " task__done" : ""}`);
     console.log(task.status, classNames)
-    // sortByClicked(task.id)
   }
 
   return (
