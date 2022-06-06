728dc17be09e7dea95a18a0cd9db621dc97cbe50
johnpatrickanders@gmail.com <66134557+johnpatrickanders@users.noreply.github.com>
2022-06-06 14:30:40 -0400

    add frontend update status

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,9 +2,18 @@ import { useState } from 'react';
 import './Task.css';
 
 export default function ({ task }) {
-  const [classNames, setClassNames] = useState("tasks__task");
-  const handleClick = () => {
+  const [classNames, setClassNames] = useState(`tasks__task ${task.status === 'Complete' ? " task__done" : ""}`);
+  const handleClick = async () => {
     task.status = task.status === 'Open' ? 'Complete' : 'Open';
+    await fetch(`tasks/${task.id}/status`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        userId: 1,
+        status: task.status,
+        taskListId: task.listId
+      })
+    })
     setClassNames(`tasks__task ${task.status === 'Complete' ? " task__done" : ""}`);
     console.log(task.status, classNames)
   }
