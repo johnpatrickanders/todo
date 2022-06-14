0ce411082f1d3045ef3f1b6baae477e40ba3d87f
johnpatrickanders@gmail.com <66134557+johnpatrickanders@users.noreply.github.com>
2022-06-14 19:43:10 -0400

    add create date to task dropdown

--- src/components/SelectedTask.js
+++ src/components/SelectedTask.js
@@ -5,7 +5,8 @@ export default function ({ task, selectedTask }) {
   return (
     selectedTask && selectedTask.id == task.id ?
       <div
-        className="task__selectedtask .hover">
+        className="task__selectedtask">
+        {selectedTask.tag}
         {selectedTask.create_date}
       </div>
       :
