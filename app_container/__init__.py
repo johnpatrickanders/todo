a10fde258eda9d51b537bd10ad88710b4632eb5c
johnpatrickanders@gmail.com <66134557+johnpatrickanders@users.noreply.github.com>
2022-08-03 14:17:28 -0400

    add /favicon route and link in index

--- app_container/__init__.py
+++ app_container/__init__.py
@@ -1,7 +1,7 @@
 import os
 from app_container.user.user_routes import user_routes
 from app_container.user.task_routes import task_routes
-from flask import Flask
+from flask import Flask, send_from_directory
 from app_container.config import ConfigApp
 from app_container.models import db, User
 from flask_migrate import Migrate
@@ -53,10 +53,7 @@ def load_user(id):
 def not_found(e):
     return app.send_static_file('index.html')
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def react_root(path):
-    print("path", path)
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
-    return app.send_static_file('index.html')
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'media'),
+                               'favicon.ico', mimetype='image/png')
